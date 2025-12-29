5e624ec2ccac56c58ce7b2ee8f0b2af55a7c8f9e
Menaga <menagashanmugam02@gmail.com>
2025-12-29 17:08:59 +0530

    change in env

--- Back-end/Routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { payBooking } = require("../Controller/paymentController");
-const authMiddleware = require("../Middleware/Token");
-
-// Dummy payment route
-router.post("/pay", authMiddleware, payBooking);
-
-module.exports = router;
