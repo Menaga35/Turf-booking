638b60bf47f8998094f1951f9c3183d07c7ac244
Menaga <menagashanmugam02@gmail.com>
2026-05-11 20:44:15 +0530

    chnages in api

--- Front-End/src/Utils/Api.js
+++ Front-End/src/Utils/Api.js
@@ -1,22 +1,22 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://turf-booking-jet-six.vercel.app/api",
+});
+
+// token
+API.interceptors.request.use((req) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user?.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
+  }
+  return req;
+});
+export default API;
 // import axios from "axios";
 
 // const API = axios.create({
 //   baseURL: "http://localhost:5000/api/users",
 // });
 
-// export const setAuthToken = (token) => {
-//   if (token) {
-//     API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//   } else {
-//     delete API.defaults.headers.common["Authorization"];
-//   }
-// };
-
 // export default API;
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:5000/api/users",
-});
-
-export default API;
