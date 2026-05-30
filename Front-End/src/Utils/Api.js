397ffbdff862ec0f5222b9cdadcf90ddda582c8b
Menaga <menagashanmugam02@gmail.com>
2026-05-30 13:24:56 +0530

    changes

--- Front-End/src/Utils/Api.js
+++ Front-End/src/Utils/Api.js
@@ -1,18 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "https://vercel.com/menagas-projects-35240fdd/turf-booking/api",
-// });
-
-// // token
-// API.interceptors.request.use((req) => {
-//   const user = JSON.parse(localStorage.getItem("user"));
-//   if (user?.token) {
-//     req.headers.Authorization = `Bearer ${user.token}`;
-//   }
-//   return req;
-// });
-// export default API;
 import axios from "axios";
 
 const API = axios.create({
