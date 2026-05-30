e8b51e4d3a5e501dae5e699b33fe98180891c162
Menaga <menagashanmugam02@gmail.com>
2026-05-30 15:00:23 +0530

    changes

--- Front-End/src/Pages/BookTurfs.jsx
+++ Front-End/src/Pages/BookTurfs.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
-import Navbar from "../Components/Navbar"; // make sure the path is correct
+import Navbar from "../Components/Navbar"; 
 import "../CSS/bookTurf.css";
 
 export default function BookTurf() {
