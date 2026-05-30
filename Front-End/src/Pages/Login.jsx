34337ac35d86f814843de13d26d23c5391cb7f74
Menaga <menagashanmugam02@gmail.com>
2026-05-30 14:30:18 +0530

    fixed frontend build

--- Front-End/src/Pages/Login.jsx
+++ Front-End/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import API from "../Utils/API.js";
-import "../CSS/Login.css";
+import "../CSS/login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
