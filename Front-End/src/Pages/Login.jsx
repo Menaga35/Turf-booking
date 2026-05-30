42c019fe5b3fec78497c17e04b1535212de0fe21
Menaga <menagashanmugam02@gmail.com>
2026-05-30 13:43:58 +0530

    changes

--- Front-End/src/Pages/Login.jsx
+++ Front-End/src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import API from "../Utils/API";
+import API from "../Utills/API";
 import "../CSS/Login.css";
 
 export default function Login() {
